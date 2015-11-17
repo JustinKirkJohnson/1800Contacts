0585fb45a3877dcd2de9ca68c1fb1a06e3e87da8
Justin Johnson <justin.kirk.johnson@gmail.com>
2015-11-16 21:54:51 -0700

    Day 2 - Toggled missing adds

---
+++ Justin.1800Contacts.Api.Test/ClassLogicTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using Justin._1800Contacts.Api.Logic;
+using Xunit;
+
+namespace Justin._1800Contacts.Api.Test
+{
+
+    public class ClassLogicTests
+    {
+        [Fact]
+        public void ClassLogic_SingleClassNoDependencySpecified_CanSortByPrerequisite()
+        {
+            // Arrange
+            ClassLogic logic = new ClassLogic();
+            List<string> inputStringArray = new List<string>
+            {
+                @"TestClass1"
+            };
+
+            // Act
+            List<string> sortedArray = logic.SortByPrerequisite(inputStringArray);
+
+            // Assert
+            Assert.Equal(1, sortedArray.Count);
+            Assert.Equal("TestClass1", sortedArray[0]);
+        }
+
+        [Fact]
+        public void ClassLogic_SingleClassEmptyDependencySpecified_CanSortByPrerequisite()
+        {
+            // Arrange
+            ClassLogic logic = new ClassLogic();
+            List<string> inputStringArray = new List<string>
+            {
+                @"TestClass1:"
+            };
+
+            // Act
+            List<string> sortedArray = logic.SortByPrerequisite(inputStringArray);
+
+            // Assert
+            Assert.Equal(1, sortedArray.Count);
+            Assert.Equal("TestClass1", sortedArray[0]);
+        }
+
+        [Fact]
+        public void ClassLogic_TwoClassesNoDependenciesSpecified_CanSortByPrerequisite()
+        {
+            // Arrange
+            ClassLogic logic = new ClassLogic();
+            List<string> inputStringArray = new List<string>
+            {
+                @"TestClass1", @"TestClass2"
+            };
+
+            // Act
+            List<string> sortedArray = logic.SortByPrerequisite(inputStringArray);
+
+            // Assert
+            Assert.Equal(2, sortedArray.Count);
+            Assert.Equal("TestClass1", sortedArray[0]);
+            Assert.Equal("TestClass2", sortedArray[1]);
+        }
+
+        [Fact]
+        public void ClassLogic_TwoClassesSingleDependency_CanSortByPrerequisite()
+        {
+            // Arrange
+            ClassLogic logic = new ClassLogic();
+            List<string> inputStringArray = new List<string>
+            {
+                @"TestClass2:TestClass1", @"TestClass1"
+            };
+
+            // Act
+            List<string> sortedArray = logic.SortByPrerequisite(inputStringArray);
+
+            // Assert
+            Assert.Equal(2, sortedArray.Count);
+            Assert.Equal("TestClass1", sortedArray[0]);
+            Assert.Equal("TestClass2", sortedArray[1]);
+        }
+
+    }
+
+}
---
+++ Justin.1800Contacts.Api/Logic/ClassLogic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Justin._1800Contacts.Api.Interface;
+
+namespace Justin._1800Contacts.Api.Logic
+{
+    public class ClassLogic : IClassLogic
+    {
+
+        public List<string> SortByPrerequisite(List<string> classDependencyList)
+        {
+            List<string> sortedClassDependencyList = new List<string>();
+            Dictionary<string, string> classDependencyMappingDictionary = 
+                new Dictionary<string, string>();
+
+            foreach (string classDependencyMapping in classDependencyList)
+            {
+                string[] classDependencyMappingComponents = 
+                    classDependencyMapping.Split(':');
+
+                string targetClass = classDependencyMappingComponents[0].Trim();
+                string prerequisiteClass = null;
+                if (classDependencyMappingComponents.Count() > 1)
+                {
+                    prerequisiteClass = classDependencyMappingComponents[1].Trim();
+                }
+
+                classDependencyMappingDictionary.Add(targetClass, prerequisiteClass);
+            }
+
+            // TODO: validate that all indicated dependencies have been specified as target classes
+
+            foreach (string targetClass in classDependencyMappingDictionary.Keys)
+            {
+                // TODO: pull into recursive method
+                string prerequisiteClass = classDependencyMappingDictionary[targetClass];
+                if (prerequisiteClass == null)
+                {
+                    sortedClassDependencyList.Insert(0, targetClass);
+                    continue;
+                }
+
+                int prerequisiteIndex = 
+                    sortedClassDependencyList.FindIndex(
+                        classItem => classItem == prerequisiteClass);
+
+                if (prerequisiteIndex == -1)
+                {
+                    sortedClassDependencyList.Add(targetClass);
+                }
+                else
+                {
+                    sortedClassDependencyList.Insert(prerequisiteIndex, targetClass);
+                }
+
+            }
+
+            return sortedClassDependencyList;
+        }
+
+    }
+}
